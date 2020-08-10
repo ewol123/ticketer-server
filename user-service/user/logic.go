7311007391f78d2adf1eaaaa9d0317bb9bf00001
Gyulavári Peti <gypeti23@gmail.com>
2020-08-10 20:04:54 +0200

    feat: initial user logic

---
+++ user-service/user/logic.go
@@ -0,0 +1,42 @@
+package user
+
+import (
+	"errors"
+	"time"
+
+	validate "gopkg.in/dealancer/validate.v2"
+
+	errs "github.com/pkg/errors"
+)
+
+// errors
+var (
+	ErrUserNotFound = errors.New("user Not Found")
+	ErrUserInvalid  = errors.New("user Invalid")
+)
+
+type userService struct {
+	userRepo Repository
+}
+
+// NewUserService : create a new user service
+func NewUserService(userRepo Repository) Service {
+	return &userService{
+		userRepo,
+	}
+}
+
+func (u *userService) Find(id int) (*User, error) {
+	return u.Find(id)
+}
+
+func (u *userService) Store(user *User) error {
+	if err := validate.Validate(user); err != nil {
+		return errs.Wrap(ErrUserInvalid, "service.User.Store")
+	}
+	user.CreatedAt = time.Now().UTC().Unix()
+	user.UpdatedAt = time.Now().UTC().Unix()
+
+	return u.userRepo.Store(user)
+
+}
