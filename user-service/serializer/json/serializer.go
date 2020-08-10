5c72a286d1bfcf950d2bb76ddd0cb24fe3447545
Gyulavári Peti <gypeti23@gmail.com>
2020-08-10 20:05:38 +0200

    feat: initial json serializer

---
+++ user-service/serializer/json/serializer.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	"encoding/json"
+
+	"github.com/ewol123/ticketer-server/user-service/user"
+	"github.com/pkg/errors"
+)
+
+// User : define our user struct so we can implement user serializer interface on it
+type User struct{}
+
+// Decode : decode our input to a User
+func (r *User) Decode(input []byte) (*user.User, error) {
+	user := &user.User{}
+	if err := json.Unmarshal(input, user); err != nil {
+		return nil, errors.Wrap(err, "serializer.User.Decode")
+	}
+	return user, nil
+}
+
+// Encode : encode our input to bytes
+func (r *User) Encode(input *user.User) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serializer.User.Encode")
+	}
+	return rawMsg, nil
+}
