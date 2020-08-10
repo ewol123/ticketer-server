700396584b37b820a3b97a5ad124992ddf2f7e5b
Gyulavári Peti <gypeti23@gmail.com>
2020-08-10 20:06:06 +0200

    feat: initial http api

---
+++ user-service/api/http.go
@@ -0,0 +1,97 @@
+package api
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/pkg/errors"
+
+	js "github.com/ewol123/ticketer-server/user-service/serializer/json"
+	"github.com/ewol123/ticketer-server/user-service/user"
+)
+
+// UserHandler : UserHandler interface with Get and Post methods
+type UserHandler interface {
+	Get(http.ResponseWriter, *http.Request)
+	Post(htp.ResponseWriter, *http.Request)
+}
+
+type handler struct {
+	userService user.Service
+}
+
+func setupResponse(w http.ResponseWriter, contentType string, body []byte, statusCode int) {
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(statusCode)
+	_, err := w.Write(body)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (h *handler) serializer(contentType string) user.Serializer {
+	/*if contentType = "application/x-msgpack" {
+		return &ms.Redirect{}
+	}*/
+	return &js.User{}
+}
+
+// NewHandler : returns a new UserHandler
+func NewHandler(userService user.UserService) UserHandler {
+	return &handler{userService: userService}
+}
+
+// Get : get a single user by id
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	contentType := r.Header.Get("Content-Type")
+	user, err := h.userService.Find(id)
+	if err != nil {
+		if errors.Cause(err) == user.ErrUserNotFound {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	responseBody, err := h.serializer(contentType).Encode(user)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	setupResponse(w, contentType, responseBody, http.StatusOK)
+}
+
+// Post : add a new user to the database
+func (h *handler) Post(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("Content-Type")
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	user, err := h.serializer(contentType).Decode(requestBody)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	err = h.userService.Store(user)
+	if err != nil {
+		if errors.Cause(err) == user.ErrUserInvalid {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	responseBody, err := h.serializer(contentType).Encode(user)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	setupResponse(w, contentType, responseBody, http.StatusCreated)
+}
