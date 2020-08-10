fcf387f1b67ea0e95ca7dd75747f471875b251e3
Gyulavári Peti <gypeti23@gmail.com>
2020-08-10 20:04:11 +0200

    feat: user model initial

---
+++ user-service/user/model.go
@@ -0,0 +1,17 @@
+package user
+
+// User : user model
+type User struct {
+	Id        int    `db:id json:id`
+	CreatedAt int64  `db:created_at json:created_at`
+	UpdatedAt int64  `db:updated_at json:updated_at`
+	FullName  string `db:full_name json:full_name validate:"empty=false`
+	Email     string `db:email json:email validate:"empty=false & format=email`
+	Password  string `db:password json:password validate:"empty=false`
+	Roles     []Role `json:roles validate:"empty=false`
+}
+
+type Role struct {
+	Id   int    `json:id db:id`
+	name string `json:name db:name`
+}
