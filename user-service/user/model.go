cbab6b1da1ebf4bccbf1715fc8622c174241ce8c
Gyulavári Peti <gypeti23@gmail.com>
2020-08-12 20:30:22 +0200

    fix: user model tags

--- user-service/user/model.go
+++ user-service/user/model.go
@@ -2,16 +2,16 @@ package user
 
 // User : user model
 type User struct {
-	Id        string    `db:id json:id`
-	CreatedAt int64  `db:created_at json:created_at`
-	UpdatedAt int64  `db:updated_at json:updated_at`
-	FullName  string `db:full_name json:full_name validate:"empty=false`
-	Email     string `db:email json:email validate:"empty=false & format=email`
-	Password  string `db:password json:password validate:"empty=false`
-	Roles     []Role `json:roles validate:"empty=false`
+	Id        string    `db:"id" json:"id"`
+	CreatedAt int64  `db:"created_at" json:"created_at"`
+	UpdatedAt int64  `db:"updated_at" json:"updated_at"`
+	FullName  string `db:"full_name" json:"full_name" validate:"empty=false"`
+	Email     string `db:"email" json:"email" validate:"empty=false & format=email"`
+	Password  string `db:"password" json:"password" validate:"empty=false"`
+	Roles     []Role `json:"roles" validate:"empty=false"`
 }
 
 type Role struct {
-	Id   string    `json:id db:id`
-	name string `json:name db:name`
+	Id   string    `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
 }
