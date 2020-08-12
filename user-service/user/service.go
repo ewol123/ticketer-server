a8d191911aeed525073202a494b44c34a2ccae9e
Gyulavári Peti <gypeti23@gmail.com>
2020-08-12 20:29:34 +0200

    feat: add more methods to repository and service interfaces
    FindAll -> pagination
    Update
    Delete

--- user-service/user/service.go
+++ user-service/user/service.go
@@ -3,5 +3,8 @@ package user
 // UserService : main service interface for user
 type Service interface {
 	Find(id string) (*User, error)
+	FindAll(page int, rowsPerPage int, sortBy string, descending bool) (*[]User, int, error)
 	Store(user *User) error
+	Update(user *User) (*User, error)
+	Delete(id string) error
 }
