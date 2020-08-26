658cda5831cb4e1e598f03f263bd13a9e00758f4
Gyulavári Peti <gypeti23@gmail.com>
2020-08-26 20:25:54 +0200

    refactor: repository, service interface function param, return val changes

--- user-service/user/repository.go
+++ user-service/user/repository.go
@@ -3,8 +3,8 @@ package user
 // UserRepository: interface to connect our business logic to our repository
 type Repository interface {
 	Find(id string) (*User, error)
-	FindAll(page int, rowsPerPage int, sortBy string, descending bool) (*[]User, int, error)
-	Store(user *User) error
+	FindAll(page int, rowsPerPage int, sortBy string, descending bool, filter string) (*[]User, int, error)
+	Store(user *User) (*User, error)
 	Update(user *User) error
 	Delete(id string) error
 }
--- user-service/user/service.go
+++ user-service/user/service.go
@@ -3,8 +3,8 @@ package user
 // UserService : main service interface for user
 type Service interface {
 	Find(id string) (*User, error)
-	FindAll(page int, rowsPerPage int, sortBy string, descending bool) (*[]User, int, error)
-	Store(user *User) error
+	FindAll(page int, rowsPerPage int, sortBy string, descending bool, filter string) (*[]User, int, error)
+	Store(user *User) (*User, error)
 	Update(user *User) error
 	Delete(id string) error
 }
