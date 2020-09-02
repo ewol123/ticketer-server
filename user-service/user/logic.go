567b13ae96792247d0c64c139ecdcf71166f524a
Gyulavári Peti <gypeti23@gmail.com>
2020-09-02 20:39:56 +0200

    refactor: user logic & tests

--- user-service/user/logic.go
+++ user-service/user/logic.go
@@ -5,19 +5,25 @@ import (
 	"errors"
 	"github.com/google/uuid"
 	errs "github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
 	validate "gopkg.in/dealancer/validate.v2"
 	"time"
 )
+
 
 // errors
 var (
 	ErrUserNotFound = errors.New("user Not Found")
 	ErrUserInvalid  = errors.New("user Invalid")
+	ErrRequestInvalid = errors.New("request payload is invalid")
 )
+
 
 type userService struct {
 	userRepo Repository
 }
+
+
 
 // NewUserService : create a new user service
 func NewUserService(userRepo Repository) Service {
@@ -26,54 +32,280 @@ func NewUserService(userRepo Repository) Service {
 	}
 }
 
-func (u *userService) Find(id string) (*User, error) {
-	return u.userRepo.Find(id)
-}
 
-func (u *userService) FindAll(page int, rowsPerPage int, sortBy string, descending bool, filter string) (*[]User, int, error) {
-	return u.userRepo.FindAll(page,rowsPerPage,sortBy,descending, filter)
-}
-
-func (u *userService) Store(user *User) (*User, error) {
-	if err := validate.Validate(user); err != nil {
-		return nil, errs.Wrap(ErrUserInvalid, "service.User.Store")
+func (u *userService) GetUser(model *GetUserRequestModel) (*GetUserResponseModel, error) {
+	if err := validate.Validate(model); err != nil {
+		return nil, errs.Wrap(ErrRequestInvalid, "service.User.GetUser")
 	}
 
-	user.CreatedAt = time.Now()
-	user.UpdatedAt = time.Now()
-	user.Status = PENDING
-
-	regCode, err := genCode(6)
+	user, err := u.userRepo.Find("id",model.Id)
 	if err != nil {
-		return nil, errs.Wrap(err, "service.User.Store")
+		return nil, errs.Wrap(err, "service.User.GetUser")
 	}
-	user.RegistrationCode = regCode
+
+	getUserResponseModel := GetUserResponseModel{
+		Id:        user.Id,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		FullName:  user.FullName,
+		Email:     user.Email,
+		Status:    user.Status,
+		RegistrationCode: user.RegistrationCode,
+		ResetPasswordCode: user.ResetPasswordCode,
+		Roles:     user.Roles,
+	}
+
+	return &getUserResponseModel, nil
+
+}
+
+
+func (u *userService) GetAllUser(model *GetAllUserRequestModel) (*GetAllUserResponseModel, error) {
+	if err := validate.Validate(model); err != nil {
+		return nil, errs.Wrap(ErrRequestInvalid, "service.User.GetAllUser")
+	}
+
+	page := model.Page
+	rowsPerPage := model.RowsPerPage
+	sortBy := model.SortBy
+	descending := model.Descending
+	filter := model.Filter
+
+	users,count,err := u.userRepo.FindAll(page,rowsPerPage,sortBy,descending,filter)
+
+	if err != nil {
+		return nil, errs.Wrap(err, "service.User.GetAllUser")
+	}
+
+	var getUserResponseModels []GetUserResponseModel
+
+	if len(*users) > 0 {
+		for _,user := range *users {
+			gUserModel := GetUserResponseModel{
+				Id:       user.Id,
+				CreatedAt: user.CreatedAt,
+				UpdatedAt: user.UpdatedAt,
+				FullName:  user.FullName,
+				Email:     user.Email,
+				Status:    user.Status,
+				Roles:     user.Roles,
+			}
+			getUserResponseModels = append(getUserResponseModels,gUserModel)
+		}
+	}
+
+	getAllUserResponseModel := GetAllUserResponseModel{
+		count: count,
+		rows:  getUserResponseModels,
+	}
+
+	return &getAllUserResponseModel, nil
+
+}
+
+
+func (u *userService) Register(model *RegisterRequestModel) (*RegisterReturnModel,error) {
+	if err := validate.Validate(model); err != nil {
+		return nil, errs.Wrap(ErrRequestInvalid, "service.User.Register")
+	}
 
 	id, err := uuid.NewRandom()
 	if err != nil {
-		return nil, errs.Wrap(err, "service.User.Store")
+		return nil, errs.Wrap(err, "service.User.Register")
 	}
-	user.Id = id.String()
-	return u.userRepo.Store(user)
+
+	regCode, err := genCode(6)
+	if err != nil {
+		return  nil, errs.Wrap(err, "service.User.Register")
+	}
+
+	password := []byte(model.Password)
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		return nil, errs.Wrap(ErrUserInvalid, "service.User.Register")
+	}
+
+	user := User{
+		Id:                id.String(),
+		CreatedAt:         time.Now(),
+		UpdatedAt:         time.Now(),
+		FullName:          model.FullName,
+		Email:             model.Email,
+		Password:          string(hashedPassword),
+		Status:            PENDING,
+		RegistrationCode:  regCode,
+		Roles:             []Role{USER},
+	}
+
+	_, err = u.userRepo.Store(&user)
+
+	regReturnModel := RegisterReturnModel{
+		Id: id.String(),
+		RegistrationCode: regCode,
+	}
+
+	return &regReturnModel, nil
+}
+
+func (u *userService) ConfirmRegistration(model *ConfirmRegistrationRequestModel) error {
+	if err := validate.Validate(model); err != nil {
+		return errs.Wrap(ErrRequestInvalid, "service.User.ConfirmRegistration")
+	}
+
+	user, err := u.userRepo.Find("email",model.Email)
+	if err != nil {
+		return  errs.Wrap(err, "service.User.ConfirmRegistration")
+	}
+
+	if user.RegistrationCode != model.RegistrationCode {
+		return errs.Wrap(ErrUserNotFound, "service.User.ConfirmRegistration")
+	}
+
+	updateUser := User{
+		Id: user.Id,
+		UpdatedAt: time.Now(),
+		Status: ACTIVE,
+		RegistrationCode: "",
+	}
+
+	err = u.userRepo.Update(&updateUser)
+
+	if err != nil {
+		return errs.Wrap(err, "service.User.ConfirmRegistration")
+	}
+	return nil
+}
+
+func (u *userService) Login(model *LoginRequestModel) (*LoginResponseModel, error) {
+	if err := validate.Validate(model); err != nil {
+		return nil, errs.Wrap(ErrRequestInvalid, "service.User.Login")
+	}
+
+	user, err := u.userRepo.Find("email",model.Email)
+
+	if err != nil {
+		return  nil, errs.Wrap(ErrUserNotFound, "service.User.Login")
+	}
+
+	if user.Status != ACTIVE {
+		return nil, errs.Wrap(ErrUserInvalid, "service.User.Login")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(model.Password))
+	if err != nil {
+		return nil, errs.Wrap(ErrUserInvalid, "service.User.Login")
+	}
+
+	respModel := LoginResponseModel{Roles: user.Roles}
+
+	return &respModel, nil
 
 }
 
-func (u *userService) Update(user *User) error {
-	if err := validate.Validate(user); err != nil {
-		return errs.Wrap(ErrUserInvalid, "service.User.Store")
+func (u *userService) SendPasswdReset(model *SendPasswdResetRequestModel) (*SendPasswdResetResponseModel,error) {
+	if err := validate.Validate(model); err != nil {
+		return nil,errs.Wrap(ErrRequestInvalid, "service.User.SendPasswdReset")
 	}
 
-	user.UpdatedAt = time.Now()
-	return u.userRepo.Update(user)
+	user, err := u.userRepo.Find("email",model.Email)
+
+	if err != nil {
+		return nil, errs.Wrap(ErrUserNotFound, "service.User.SendPasswdReset")
+	}
+
+	if user.Status != ACTIVE {
+		return nil, errs.Wrap(ErrUserInvalid, "service.User.SendPasswdReset")
+	}
+
+	regCode, err := genCode(6)
+	if err != nil {
+		return  nil, errs.Wrap(err, "service.User.SendPasswdReset")
+	}
+
+	updateUser := User{
+		Id: user.Id,
+		UpdatedAt:         time.Now(),
+		ResetPasswordCode: regCode,
+	}
+
+	err = u.userRepo.Update(&updateUser)
+
+	if err != nil {
+		return nil, errs.Wrap(err, "service.User.SendPasswdReset")
+	}
+
+	pwdResetResponseModel := SendPasswdResetResponseModel{
+		ResetPasswordCode: regCode,
+		Email: user.Email,
+		FullName: user.FullName}
+
+	return &pwdResetResponseModel, nil
 }
 
-func (u *userService) Delete(id string) error {
-	return u.userRepo.Delete(id)
+
+func (u *userService) ResetPassword(model *ResetPasswordRequestModel) error {
+	if err := validate.Validate(model); err != nil {
+		return errs.Wrap(ErrRequestInvalid, "service.User.ResetPassword")
+	}
+
+	user, err := u.userRepo.Find("email",model.Email)
+	if err != nil {
+		return  errs.Wrap(err, "service.User.ResetPassword")
+	}
+
+	if user.ResetPasswordCode != model.ResetPasswordCode {
+		return errs.Wrap(ErrUserNotFound, "service.User.ResetPassword")
+	}
+
+	password := []byte(model.Password)
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		return errs.Wrap(ErrUserInvalid, "service.User.ResetPassword")
+	}
+
+	updateUser := User{
+		Id: user.Id,
+		UpdatedAt: time.Now(),
+		ResetPasswordCode: "",
+		Password: string(hashedPassword),
+	}
+
+	err = u.userRepo.Update(&updateUser)
+
+	if err != nil {
+		return errs.Wrap(err, "service.User.ConfirmRegistration")
+	}
+	return nil
+
+}
+
+func (u *userService) UpdateUser(model *UpdateUserRequestModel) error {
+	if err := validate.Validate(model); err != nil {
+		return errs.Wrap(ErrRequestInvalid, "service.User.UpdateUser")
+	}
+
+	user := User{
+		Id: 			  model.Id,
+		UpdatedAt:        time.Now(),
+		FullName:         model.FullName,
+		Email:            model.Email,
+		Status:           model.Status,
+	}
+
+	return u.userRepo.Update(&user)
+}
+
+
+func (u *userService) DeleteUser(model *DeleteUserRequestModel) error {
+	if err := validate.Validate(model); err != nil {
+		return errs.Wrap(ErrRequestInvalid, "service.User.DeleteUser")
+	}
+	return u.userRepo.Delete(model.Id)
 }
 
 
 // UTILITY
-const otpChars = "1234567890"
+const codeCars = "1234567890"
 func genCode(length int) (string, error) {
 	buffer := make([]byte, length)
 	_, err := rand.Read(buffer)
@@ -81,9 +313,9 @@ func genCode(length int) (string, error) {
 		return "", err
 	}
 
-	otpCharsLength := len(otpChars)
+	otpCharsLength := len(codeCars)
 	for i := 0; i < length; i++ {
-		buffer[i] = otpChars[int(buffer[i])%otpCharsLength]
+		buffer[i] = codeCars[int(buffer[i])%otpCharsLength]
 	}
 
 	return string(buffer), nil
--- user-service/user/logic_test.go
+++ user-service/user/logic_test.go
@@ -1,32 +1,79 @@
 package user
 
 import (
+	"github.com/pkg/errors"
 	"reflect"
+	"strings"
 	"testing"
 )
-
-var user = User{
-	Id:        "8a5e9658-f954-45c0-a232-4dcbca0d4907",
-	FullName:  "Test User",
-	Email:     "test.user@test.com",
-	Password:  "bcrypt",
-}
-
-
-var users = []User{
-	user,
-}
 
 var roles  = []Role{
 	{ Id: "72daf87a-fda4-4c72-aff9-85edd68d155f", Name: "user" },
 	{ Id: "336a3ff6-9fdb-496f-ac8c-e37759969cf2", Name: "admin" },
 }
 
+var user = User{
+	Id:        "8a5e9658-f954-45c0-a232-4dcbca0d4907",
+	FullName:  "Test User",
+	Email:     "test.user@test.com",
+	Password:  "$2y$10$2wcRIg0zbCk8b02HVgb3bui6Dkd8xeCZEmBhAbY8yfJ8NtzbzABk2",
+	RegistrationCode: "212345",
+	ResetPasswordCode: "123456",
+	Status: ACTIVE,
+	Roles: roles,
+}
+
+var user2 = User{
+	Id:        "2a5e9658-f954-45c0-a232-4dcbca0d4904",
+	FullName:  "Test User",
+	Email:     "test.user2@test.com",
+	Password:  "bcrypt",
+	RegistrationCode: "111111",
+	ResetPasswordCode: "333333",
+	Status: ACTIVE,
+}
+
+var user3 = User{
+	Id:        "2a5e9658-d954-45c0-a232-4dcbca0d4904",
+	FullName:  "Test User",
+	Email:     "test.user3@test.com",
+	Password:  "something",
+	RegistrationCode: "111111",
+	ResetPasswordCode: "333333",
+	Status: ACTIVE,
+}
+
+var user4 = User {
+	Id:        "2a5e9658-d954-45c0-a232-2dcbca0d4904",
+	FullName:  "Test User",
+	Email:     "test.user@resetpw.com",
+	Password:  "something",
+	RegistrationCode: "111111",
+	ResetPasswordCode: "333333",
+	Status: ACTIVE,
+}
+
+
+var users = []User{
+	user,
+	user2,
+	user3,
+	user4,
+}
+
+
 type newRepo struct {}
 
-func (r* newRepo) Find(id string) (*User, error) {
+func (r* newRepo) Find(column string, value string) (*User, error) {
+
+	//hack for tests
+	firstUpper := strings.Title(column)
+
 	for i := range users {
-		if users[i].Id == id {
+		user := reflect.ValueOf(&users[i])
+
+		colVal := reflect.Indirect(user).FieldByName(firstUpper)
+		if colVal.String() == value {
 			return &users[i], nil
 		}
 	}
@@ -37,7 +84,7 @@ func (r* newRepo) FindAll(page int, rowsPerPage int, sortBy string, descending b
 	offsetPage := page - 1
 
 	if rowsPerPage * page > len(users) -1 {
-		return nil, len(users), nil
+		return &users, len(users), nil
 	}
 
 	pagination := users[offsetPage * rowsPerPage: page * rowsPerPage]
@@ -101,83 +148,280 @@ func TestNewUserService(t *testing.T) {
 	}
 }
 
-func TestFind(t *testing.T){
+func TestGetUser(t *testing.T){
 	r := &newRepo{}
 	service := NewUserService(r)
 
-	shouldFind, err := service.Find("8a5e9658-f954-45c0-a232-4dcbca0d4907")
+	getUserModel := GetUserRequestModel{Id: "8a5e9658-f954-45c0-a232-4dcbca0d4907" }
+
+	shouldFind, err := service.GetUser(&getUserModel)
 
 	if err != nil {
-		t.Errorf("test if found failed, expected %v, got %v", user, err)
+		t.Errorf("test found failed, expected %v, got %v", user, err)
 	}
 
 	if shouldFind.Id != user.Id {
-		t.Errorf("test if found failed, expected %v, got %v", user, shouldFind)
+		t.Errorf("test found failed, expected %v, got %v", user, shouldFind)
 	} else {
-		t.Logf("test if found success, expected %v, got %v", user, shouldFind)
+		t.Logf("test found success, expected %v, got %v", user, shouldFind)
 	}
 
-	_, err = service.Find("abc")
+	wrongModel := GetUserRequestModel{Id: "abc" }
+
+	_, err = service.GetUser(&wrongModel)
 
 	if err != nil {
-		if err != ErrUserNotFound {
-			t.Errorf("test if not found failed, expected %v, got %v", ErrUserNotFound, err)
+		if errors.Cause(err) != ErrRequestInvalid {
+			t.Errorf("test not found failed, expected %v, got %v", ErrRequestInvalid, err)
 		} else {
-			t.Logf("test if not found success, expected %v, got %v", ErrUserNotFound, err)
+			t.Logf("test not found success, expected %v, got %v", ErrRequestInvalid, err)
 		}
 	}
 }
 
-func TestStore(t *testing.T) {
+func TestRegister(t *testing.T) {
 	r := &newRepo{}
 	service := NewUserService(r)
 
-	newUser := User{
-		Id:        "94996a7a-312d-405b-9376-eb1850359632",
+	newUser := RegisterRequestModel{
 		FullName:  "test 2",
 		Email:     "test2@test.com",
 		Password:  "bcrypt2",
-		Roles:     roles,
 	}
 
-	invalidUser := User{
+	invalidUser := RegisterRequestModel{
 		FullName: "hallo",
 	}
 	
-	user, err := service.Store(&newUser)
+	user, err := service.Register(&newUser)
 
 	if err != nil{
-		t.Errorf("test user store failed, expected %v, got %v", nil, err)
+		t.Errorf("test user register failed, expected %v, got %v", nil, err)
 	} else {
-		t.Logf("test user store success, expected %v, got %v", user, user)
+		t.Logf("test user register success, expected %v, got %v", user, user)
 	}
 
-	user, err = service.Store(&invalidUser)
+	user, err = service.Register(&invalidUser)
 
 	if err != nil{
-		t.Logf("test user store invalid success, expected %v, got %v", ErrUserInvalid, err)
+		t.Logf("test user register invalid success, expected %v, got %v", ErrRequestInvalid, err)
 	} else {
-		t.Errorf("test user store invalid failed, expected %v, got %v", ErrUserInvalid, err)
+		t.Errorf("test user register invalid failed, expected %v, got %v", ErrRequestInvalid, err)
 	}
 }
 
-func TestUpdate(t *testing.T) {
+
+func TestConfirmRegistration(t *testing.T) {
 	r := &newRepo{}
 	service := NewUserService(r)
 
-	updateUser := User{
-		Id:        "8a5e9658-f954-45c0-a232-4dcbca0d4907",
-		FullName:  "updated",
-		Email:     "updated@test.com",
-		Password:  "bcrypt2",
-		Roles:     roles,
+	validUser := ConfirmRegistrationRequestModel{
+		Email:     "test.user2@test.com",
+		RegistrationCode: "111111",
 	}
 
-	invalidUser := User{
+	invalidUser := ConfirmRegistrationRequestModel{
+		Email: "hallo",
+	}
+
+	 err := service.ConfirmRegistration(&validUser)
+
+	if err != nil{
+		t.Errorf("test user confirm registration failed, expected %v, got %v", nil, err)
+	} else {
+		t.Logf("test user confirm registration success, expected %v, got %v", nil, nil)
+	}
+
+	 err = service.ConfirmRegistration(&invalidUser)
+
+	if err != nil{
+		t.Logf("test user confirm registration invalid success, expected %v, got %v", ErrRequestInvalid, err)
+	} else {
+		t.Errorf("test user confirm registration invalid failed, expected %v, got %v", ErrRequestInvalid, err)
+	}
+}
+
+
+func TestGetAllUser(t *testing.T){
+	r := &newRepo{}
+	service := NewUserService(r)
+
+	getAllUserModel := GetAllUserRequestModel{
+		Page:        1,
+		RowsPerPage: 10,
+		SortBy:      "Id",
+		Descending:  false,
+		Filter:      "",
+	}
+
+	shouldFind, err := service.GetAllUser(&getAllUserModel)
+
+	if err != nil {
+		t.Errorf("test GetAllUser failed, expected %v, got %v", "GetAllUserResponseModel", err)
+	}
+
+	if shouldFind.rows[0].Id != "8a5e9658-f954-45c0-a232-4dcbca0d4907" {
+		t.Errorf("test GetAllUser failed, expected %v, got %v", "8a5e9658-f954-45c0-a232-4dcbca0d4907", shouldFind.rows[0].Id)
+	} else {
+		t.Logf("test GetAllUser success, expected %v, got %v", "8a5e9658-f954-45c0-a232-4dcbca0d4907",  shouldFind.rows[0].Id)
+	}
+
+	if shouldFind.count > 0 {
+		t.Logf("test GetALlUser success, expected %v, got %v", "greater than zero", shouldFind.count)
+	} else {
+		t.Errorf("test GetAllUser failed, expected %v, got %v", "greater than zero", shouldFind.count)
+	}
+
+
+	wrongModel := GetAllUserRequestModel{
+		RowsPerPage: 0,
+		SortBy:      "",
+		Descending:  false,
+		Filter:      "",
+	}
+
+	_, err = service.GetAllUser(&wrongModel)
+
+	if err != nil {
+		if errors.Cause(err) != ErrRequestInvalid {
+			t.Errorf("test GetAllUser failed, expected %v, got %v", ErrRequestInvalid, err)
+		} else {
+			t.Logf("test GetAllUser success, expected %v, got %v", ErrRequestInvalid, err)
+		}
+	}
+}
+
+func TestLogin(t *testing.T){
+	r := &newRepo{}
+	service := NewUserService(r)
+
+	loginModel := LoginRequestModel{
+		Email:    "test.user@test.com",
+		Password: "bcryptasdasd123",
+	}
+
+	loginResp, err := service.Login(&loginModel)
+
+	if err != nil {
+		t.Errorf("test Login failed, expected %v, got %v", nil, err)
+	} else {
+		t.Logf("test Login success, expected %v, got %v", nil, err)
+	}
+
+	if loginResp.Roles[0].Name != "user" {
+		t.Errorf("test login failed, expected role %v, got %v","user", loginResp.Roles[0].Name)
+	} else {
+		t.Logf("test Login success, expected role %v, got %v", "user", loginResp.Roles[0].Name)
+	}
+
+	badLoginModel := LoginRequestModel{
+		Email:    "asdsad",
+	}
+
+	loginResp, err = service.Login(&badLoginModel)
+
+	if err != nil {
+		if errors.Cause(err) == ErrRequestInvalid {
+			t.Logf("test Login success, expected %v, got %v",ErrRequestInvalid, err)
+		} else {
+			t.Errorf("test Login failed, expected %v, got %v", ErrRequestInvalid, err)
+		}
+	}
+
+	wrongPwModel := LoginRequestModel{
+		Email:    "test.user@test.com",
+		Password: "abaslé3",
+	}
+
+	loginResp, err = service.Login(&wrongPwModel)
+
+	if err != nil {
+		if errors.Cause(err) == ErrUserInvalid {
+			t.Logf("test Login success, expected %v, got %v",ErrUserInvalid, err)
+		} else {
+			t.Errorf("test Login failed, expected %v, got %v", ErrUserInvalid, err)
+		}
+	}
+}
+
+func TestSendPasswdReset(t *testing.T){
+	r := &newRepo{}
+	service := NewUserService(r)
+
+	newUser := SendPasswdResetRequestModel{
+		Email:     "test.user3@test.com",
+	}
+
+	invalidUser := SendPasswdResetRequestModel{
+		Email: "asd",
+	}
+
+	user, err := service.SendPasswdReset(&newUser)
+
+	if err != nil{
+		t.Errorf("test user SendPasswdReset failed, expected %v, got %v", nil, err)
+	} else {
+		t.Logf("test user SendPasswdReset success, expected %v, got %v", "SendPasswdResetResponseModel", user)
+	}
+
+	user, err = service.SendPasswdReset(&invalidUser)
+
+	if err != nil{
+		t.Logf("test user SendPasswdReset invalid success, expected %v, got %v", ErrRequestInvalid, err)
+	} else {
+		t.Errorf("test user SendPasswdReset invalid failed, expected %v, got %v", ErrRequestInvalid, err)
+	}
+
+}
+
+func TestResetPassword(t *testing.T){
+	r := &newRepo{}
+	service := NewUserService(r)
+
+	validUser := ResetPasswordRequestModel{
+		Email:     "test.user@resetpw.com",
+		ResetPasswordCode: "333333",
+		Password: "asdasd1234",
+	}
+
+	invalidUser := ResetPasswordRequestModel{
+		Email: "hallo",
+	}
+
+	err := service.ResetPassword(&validUser)
+
+	if err != nil{
+		t.Errorf("test user ResetPassword failed, expected %v, got %v", nil, err)
+	} else {
+		t.Logf("test user ResetPassword success, expected %v, got %v", nil, nil)
+	}
+
+	err = service.ResetPassword(&invalidUser)
+
+	if err != nil{
+		t.Logf("test user ResetPassword invalid success, expected %v, got %v", ErrRequestInvalid, err)
+	} else {
+		t.Errorf("test user ResetPassword invalid failed, expected %v, got %v", ErrRequestInvalid, err)
+	}
+
+}
+
+func TestUpdateUser(t *testing.T) {
+	r := &newRepo{}
+	service := NewUserService(r)
+
+
+	updateUser := UpdateUserRequestModel{
+		Id: "8a5e9658-f954-45c0-a232-4dcbca0d4907",
+		FullName:  "updated",
+		Email:     "updated@test.com",
+	}
+
+	invalidUser := UpdateUserRequestModel{
 		FullName: "hallo",
 	}
 
-	err := service.Update(&updateUser)
+	err := service.UpdateUser(&updateUser)
 
 	if err != nil{
 		t.Errorf("test user update failed, expected %v, got %v", nil, err)
@@ -185,8 +429,11 @@ func TestUpdate(t *testing.T) {
 		t.Logf("test user update success, expected %v, got %v", nil, err)
 	}
 
+	gUserReqModel := GetUserRequestModel{
+		Id: "8a5e9658-f954-45c0-a232-4dcbca0d4907",
+	}
 
-	shouldFind, err := service.Find("8a5e9658-f954-45c0-a232-4dcbca0d4907")
+	shouldFind, err := service.GetUser(&gUserReqModel)
 
 	if err != nil {
 		t.Errorf("test user update failed, expected %v after update, got %v",updateUser, shouldFind)
@@ -197,13 +444,10 @@ func TestUpdate(t *testing.T) {
 		if shouldFind.Email != "updated@test.com"{
 			t.Errorf("test user update failed, expected Email = %v after update, got %v",updateUser.Email, shouldFind.Email)
 		}
-		if shouldFind.Password != "bcrypt2" {
-			t.Errorf("test user update failed, expected Password = %v after update, got %v",updateUser.Password, shouldFind.Password)
-		}
 			t.Logf("test user update success, all values are as expected")
 	}
 
-	err = service.Update(&invalidUser)
+	err = service.UpdateUser(&invalidUser)
 
 	if err != nil{
 		t.Logf("test user update invalid success, expected %v, got %v", ErrUserInvalid, err)
@@ -212,11 +456,13 @@ func TestUpdate(t *testing.T) {
 	}
 }
 
-func TestDelete(t *testing.T){
+func TestDeleteUser(t *testing.T){
 	r := &newRepo{}
 	service := NewUserService(r)
 
-	err := service.Delete("8a5e9658-f954-45c0-a232-4dcbca0d4907")
+	deleteUserModel := DeleteUserRequestModel{Id:"8a5e9658-f954-45c0-a232-4dcbca0d4907" }
+
+	err := service.DeleteUser(&deleteUserModel)
 
 	if err != nil {
 		t.Errorf("test delete failed, expected %v, got %v", nil, err)
@@ -225,13 +471,15 @@ func TestDelete(t *testing.T){
 	}
 
 
-	err = service.Delete("abc")
+	invalidModel := DeleteUserRequestModel{Id:"abc" }
+
+	err = service.DeleteUser(&invalidModel)
 
 	if err != nil {
-		if err != ErrUserNotFound {
-			t.Errorf("test delete failed, expected %v, got %v", ErrUserNotFound, err)
+		if errors.Cause(err) != ErrRequestInvalid {
+			t.Errorf("test delete failed, expected %v, got %v", ErrRequestInvalid, err)
 		} else {
-			t.Logf("test delete success, expected %v, got %v", ErrUserNotFound, err)
+			t.Logf("test delete success, expected %v, got %v", ErrRequestInvalid, err)
 		}
 	}
 
