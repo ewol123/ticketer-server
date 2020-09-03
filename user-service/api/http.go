f432aba41de770d507e944afd7f369d93df1a508
Gyulavári Peti <gypeti23@gmail.com>
2020-09-03 20:08:02 +0200

    refactor: api http

--- user-service/api/http.go
+++ user-service/api/http.go
@@ -1,30 +1,30 @@
 package api
 
 import (
-	"io/ioutil"
-	"log"
-	"net/http"
-
-	"github.com/go-chi/chi"
-	"github.com/pkg/errors"
-
 	js "github.com/ewol123/ticketer-server/user-service/serializer/json"
 	"github.com/ewol123/ticketer-server/user-service/user"
+	"log"
+	"net/http"
 )
 
 // UserHandler : UserHandler interface with Get and Post methods
 type UserHandler interface {
-	Get(http.ResponseWriter, *http.Request)
-	GetAll(http.ResponseWriter, *http.Request)
-	Post(http.ResponseWriter, *http.Request)
-	Put(http.ResponseWriter, *http.Request)
-	Patch(http.ResponseWriter, *http.Request)
-	Delete(http.ResponseWriter, *http.Request)
+	GetUser(http.ResponseWriter, *http.Request)
+	GetAllUser(http.ResponseWriter, *http.Request)
+	UpdateUser(http.ResponseWriter, *http.Request)
+	DeleteUser(http.ResponseWriter, *http.Request)
+	Register(http.ResponseWriter, *http.Request)
+	ConfirmRegistration(http.ResponseWriter, *http.Request)
+	Login(http.ResponseWriter, *http.Request)
+	SendPasswdReset(http.ResponseWriter, *http.Request)
+	ResetPassword(http.ResponseWriter, *http.Request)
 }
+
 
 type handler struct {
 	userService user.Service
 }
+
 
 func setupResponse(w http.ResponseWriter, contentType string, body []byte, statusCode int) {
 	w.Header().Set("Content-Type", contentType)
@@ -35,162 +35,16 @@ func setupResponse(w http.ResponseWriter, contentType string, body []byte, statu
 	}
 }
 
+
 func (h *handler) serializer(contentType string) user.Serializer {
 	/*if contentType = "application/x-msgpack" {
-		return &ms.Redirect{}
+		return &ms.User{}
 	}*/
 	return &js.User{}
 }
 
+
 // NewHandler : returns a new UserHandler
 func NewHandler(userService user.Service) UserHandler {
 	return &handler{userService: userService}
-}
-
-func (h *handler) GetAll(writer http.ResponseWriter, request *http.Request) {
-	panic("implement me")
-}
-
-// Get : get a single user by id
-func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	contentType := r.Header.Get("Content-Type")
-	res, err := h.userService.Find(id)
-	if err != nil {
-		if errors.Cause(err) == user.ErrUserNotFound {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	responseBody, err := h.serializer(contentType).Encode(res)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	setupResponse(w, contentType, responseBody, http.StatusOK)
-}
-
-// Post : add a new user to the database
-func (h *handler) Post(w http.ResponseWriter, r *http.Request) {
-	contentType := r.Header.Get("Content-Type")
-	requestBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	userReq, err := h.serializer(contentType).Decode(requestBody)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	err = h.userService.Store(userReq)
-	if err != nil {
-		if errors.Cause(err) == user.ErrUserInvalid {
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
-		}
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	setupResponse(w, contentType, []byte{}, http.StatusCreated)
-}
-
-func (h *handler) Put(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	contentType := r.Header.Get("Content-Type")
-	requestBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	userReq, err := h.serializer(contentType).Decode(requestBody)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	userReq.Id = id
-	userReq.Roles = []user.Role{} // we should pass domain logic service validation with an empty roles slice
-	userReq.Password = ""
-
-	if userReq.FullName == "" || userReq.Email == "" {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	err = h.userService.Update(userReq)
-	if err != nil {
-		if errors.Cause(err) == user.ErrUserInvalid {
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
-		}
-		if errors.Cause(err) == user.ErrUserNotFound {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	setupResponse(w, contentType, []byte{}, http.StatusCreated)
-}
-
-func (h *handler) Patch(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	contentType := r.Header.Get("Content-Type")
-	requestBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	userReq, err := h.serializer(contentType).Decode(requestBody)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-	userReq.Id = id
-	userReq.Roles = []user.Role{} // we should pass domain logic service validation with an empty roles slice
-	userReq.Password = ""
-
-	if userReq.FullName == "" && userReq.Email == "" {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	err = h.userService.Update(userReq)
-	if err != nil {
-		if errors.Cause(err) == user.ErrUserInvalid {
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
-		}
-		if errors.Cause(err) == user.ErrUserNotFound {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	setupResponse(w, contentType, []byte{}, http.StatusCreated)
-}
-
-func (h *handler) Delete(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	contentType := r.Header.Get("Content-Type")
-	err := h.userService.Delete(id)
-	if err != nil {
-		if errors.Cause(err) == user.ErrUserNotFound {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	setupResponse(w, contentType, []byte{}, http.StatusOK)
 }
