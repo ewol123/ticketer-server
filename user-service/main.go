2aa3a5dcd56de54a26bf5df2e217a62c2cb2850d
Gyulavári Peti <gypeti23@gmail.com>
2020-09-07 21:55:28 +0200

    refactor:  move code from main to api

--- user-service/main.go
+++ user-service/main.go
@@ -1,83 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-
-	h "github.com/ewol123/ticketer-server/user-service/api"
-	pr "github.com/ewol123/ticketer-server/user-service/repository"
-
-	"github.com/ewol123/ticketer-server/user-service/user"
+	app "github.com/ewol123/ticketer-server/user-service/api/routes"
 )
 
 func main() {
-	repo := chooseRepo()
-	service := user.NewUserService(repo)
-	handler := h.NewHandler(service)
-
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	r.Get("/", handler.GetAll)
-	r.Get("/{id}", handler.Get)
-	r.Post("/", handler.Post)
-	r.Put("/{id}", handler.Put)
-	r.Patch("/{id}", handler.Patch)
-	r.Delete("/{id}", handler.Delete)
-
-
-	errs := make(chan error, 2)
-	go func() {
-		fmt.Println("Listening on port :8000")
-		errs <- http.ListenAndServe(httpPort(), r)
-
-	}()
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, syscall.SIGINT)
-		errs <- fmt.Errorf("%s", <-c)
-	}()
-
-	fmt.Printf("Terminated %s", <-errs)
-
+	app.Run()
 }
 
-func httpPort() string {
-	port := "8000"
-	if os.Getenv("PORT") != "" {
-		port = os.Getenv("PORT")
-	}
-	return fmt.Sprintf(":%s", port)
-}
 
-func chooseRepo() user.Repository {
-	var repo user.Repository
-	switch os.Getenv("URL_DB") {
-	/* case "redis":
-	redisURL := os.Getenv("REDIS_URL")
-	repo, err := rr.NewRedisRepository(redisURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	re := repo */
-	case "postgres":
-		connectionString := os.Getenv("CONNECTION_STRING")
-		pgRepo, err := pr.NewPgRepository(connectionString)
-		if err != nil {
-			log.Fatal(err)
-		}
-		repo = pgRepo
-	}
-	return repo
-}
+
