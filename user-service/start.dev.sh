439842c3acf40d59f8753278753440c43ac36eb2
Gyulavári Peti <gypeti23@gmail.com>
2020-09-07 21:59:59 +0200

    chore: add more env variables to start.dev script

--- user-service/start.dev.sh
+++ user-service/start.dev.sh
@@ -1,3 +1,5 @@
 export SENDGRID_API_KEY='YOUR_API_KEY'
 export JWT_SECRET='pass'
+export DB_TYPE ='postgres'
+export CONNECTION_STRING='user=postgres password=test dbname=user_test sslmode=disable'
 go run main.go
