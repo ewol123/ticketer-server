d68bfbf1632ea3b5dca680a7d49cb0d8834c50d8
Gyulavári Peti <gypeti23@gmail.com>
2020-09-09 20:31:41 +0200

    chore: add worker role to role seed

--- user-service/repository/postgres/seed/seed.sql
+++ user-service/repository/postgres/seed/seed.sql
@@ -1,4 +1,5 @@
 INSERT INTO role(id,name)
 VALUES
 ('72daf87a-fda4-4c72-aff9-85edd68d155f','user'),
-('336a3ff6-9fdb-496f-ac8c-e37759969cf2','admin');
+('336a3ff6-9fdb-496f-ac8c-e37759969cf2','admin'),
+('5676519f-a588-4e1b-ae65-e473c02ad14e','worker');
