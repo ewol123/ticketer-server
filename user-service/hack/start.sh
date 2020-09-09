9622029c27fef305adbc753616f2f0b395404de5
Gyulavári Peti <gypeti23@gmail.com>
2020-09-09 17:28:01 +0200

    chore: move scripts/etc to hack folder

---
+++ user-service/hack/start.sh
@@ -0,0 +1,5 @@
+export SENDGRID_API_KEY='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'
+export JWT_SECRET='somesecurepassword'
+export DB_TYPE ='postgres'
+export CONNECTION_STRING='user=postgres password=test dbname=user_test sslmode=disable'
+go run main.go
