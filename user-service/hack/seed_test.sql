95a58503a13fd0389d008a6ad54275df7fc8704f
Gyulavári Peti <gypeti23@gmail.com>
2020-09-09 20:32:43 +0200

    chore: add worker role to seed_tests

--- user-service/hack/seed_test.sql
+++ user-service/hack/seed_test.sql
@@ -1,7 +1,8 @@
 INSERT INTO role(id,name)
 VALUES
 ('72daf87a-fda4-4c72-aff9-85edd68d155f','user'),
-('336a3ff6-9fdb-496f-ac8c-e37759969cf2','admin');
+('336a3ff6-9fdb-496f-ac8c-e37759969cf2','admin'),
+('5676519f-a588-4e1b-ae65-e473c02ad14e','worker');
 
 INSERT INTO "user"
 (id,created_at,full_name,email,password,updated_at,status,registration_code,reset_password_code)
