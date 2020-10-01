4eff4f314c3bed1571919ef4551740be976baec4
Gyulavári Peti <gypeti23@gmail.com>
2020-10-01 21:32:56 +0200

    fix: [user-service] start script

--- user-service/hack/start.sh
+++ user-service/hack/start.sh
@@ -1,6 +1,6 @@
 export SENDGRID_API_KEY='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'
 export JWT_SECRET='somesecurepassword'
 export DB_TYPE='postgres'
-export CONNECTION_STRING="user=postgres password=test dbname=user_test sslmode=disable"
+export CONNECTION_STRING="user=postgres password=user dbname=user sslmode=disable"
 export PORT='80'
 go run ../main.go
