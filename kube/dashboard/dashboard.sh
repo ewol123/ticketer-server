5dc6b64aebda6266076b885bee609c8dac5ea642
Gyulavári Peti <gypeti23@gmail.com>
2020-10-12 21:52:45 +0200

    fix: k8s dashboard version

--- kube/dashboard/dashboard.sh
+++ kube/dashboard/dashboard.sh
@@ -1,4 +1,4 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
 kubectl apply -f ./serviceaccount.yaml
 kubectl get secret -n kube-system
 
--- kube/dashboard/dashborad-delete.bat
+++
@@ -1,2 +0,0 @@
-kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
-kubectl delete -f ./serviceaccount.yaml
