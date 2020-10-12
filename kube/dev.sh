24dc51410fe780463503582f34e35176abbe6bad
Gyulavári Peti <gypeti23@gmail.com>
2020-10-12 21:53:00 +0200

    fix: k8s dev.sh script

--- kube/dev.sh
+++ kube/dev.sh
@@ -1,4 +1,3 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm install ticketer-ingress ingress-nginx/ingress-nginx
-kubectl wait --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
-kubectl create -f ./app
+helm install app-ingress ingress-nginx/ingress-nginx --namespace=prod
+kubectl wait --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s -n prod
